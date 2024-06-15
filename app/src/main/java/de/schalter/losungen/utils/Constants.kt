318c71b23ae6a6394da33dfd595bf6ea3c3450ec
Benjamin <benjamin@awesomebible.de>
2024-06-15 20:55:08 +0200

    Update stuff & build action

--- app/src/main/java/de/schalter/losungen/utils/Constants.kt
+++ app/src/main/java/de/schalter/losungen/utils/Constants.kt
@@ -6,13 +6,13 @@ object Constants {
      * Use this url to get the download path for all available year and language
      */
     val urlsOverviewAvailableData: List<String> = listOf(
-//            "http://10.0.2.2/available-data.json",
-            "https://raw.githubusercontent.com/schalterDev/Losungen-Android-2/master/data-for-webserver/available-data.json",
-            "https://losungen.webdesign-schalter.de/available-data.json")
+//          "http://10.0.2.2/available-data.json",
+            "https://losungen.awesomebible.de/available-data.json",
+            "https://bennihtm.github.io/Losungen-Server/available-data.json")
 
     const val urlPrivacyWebsite = "https://losungen.webdesign-schalter.de/privacy"
-    const val programmerMail = "schalter.dev@gmail.com"
-    const val urlProgrammer = "https://webdesign-schalter.de"
-    const val urlGitHub = "https://github.com/schalterDev/Losungen-Android-2"
+    const val programmerMail = "benjamin@awesomebible.de"
+    const val urlProgrammer = "https://bendaha.eu.org"
+    const val urlGitHub = "https://github.com/bennihtm/Losungen-Android"
     const val urlTranslation = "https://poeditor.com/join/project/WWqXh1OIBE"
 }
